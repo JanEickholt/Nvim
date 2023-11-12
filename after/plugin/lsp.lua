113b15d7ccd07a6fc2f22d3628fb530cfb976829
Jan Eickholt <eickholtjan@gmail.com>
2023-11-12 15:32:30 +0100

    Initial Commit

---
+++ after/plugin/lsp.lua
@@ -0,0 +1,56 @@
+local lsp = require('lsp-zero')
+
+lsp.preset('recommended')
+
+lsp.ensure_installed({
+    'jdtls',
+    'tsserver',
+    'eslint',
+    'rust_analyzer',
+})
+
+lsp.setup_servers({'tsserver', 'eslint'})
+local cmp = require('cmp')
+local cmp_select = { behavior = cmp.SelectBehavior.Select }
+local cmp_mappings = lsp.defaults.cmp_mappings({
+    ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
+    ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
+    ['<C-o>'] = cmp.mapping.complete(),
+    ['<C-y>'] = cmp.mapping.confirm({ select = true }),
+})
+
+vim.keymap.set("n", "<leader>vd", vim.diagnostic.open_float)
+
+lsp.set_preferences({
+    sign_icons = {}
+})
+
+lsp.on_attach(function(client, bufnr)
+    local opts = { buffer = bufrn, remap = false }
+
+    vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
+    vim.keymap.set("n", "gi", function() vim.lsp.buf.implementation() end, opts)
+    vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
+    vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
+    vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
+    vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
+    vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
+    vim.keymap.set("n", "<leader>gr", function() vim.lsp.buf.references() end, opts)
+    vim.keymap.set("n", "<leader>rn", function() vim.lsp.buf.rename() end, opts)
+    vim.keymap.set("i", "<C-s>", function() vim.lsp.buf.signature_help() end, opts)
+end)
+
+
+lsp.setup()
+vim.diagnostic.config {
+    underline = true,
+    virtual_text = {
+        prefix = "",
+        severity = nil,
+        source = "if_many",
+        format = nil,
+    },
+    signs = true,
+    severity_sort = true,
+    update_in_insert = false,
+}
---
+++ after/plugin/telescope.lua
@@ -0,0 +1,20 @@
+require('telescope').setup({
+    defaults = {
+        layout_strategy = 'horizontal'
+    }
+})
+
+require('telescope').load_extension('fzf')
+
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+vim.keymap.set('n', '<C-p>', builtin.git_files, {})
+vim.keymap.set('n', '<C-g>', builtin.git_status, {})
+vim.keymap.set('n', '<C-l>', builtin.git_commits, {})
+vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+vim.keymap.set('n', '<leader>fw', builtin.diagnostics, {})
+vim.keymap.set('n', '<leader>ps', function()
+	builtin.grep_string({ search = vim.fn.input("Grep > ")});
+end)
